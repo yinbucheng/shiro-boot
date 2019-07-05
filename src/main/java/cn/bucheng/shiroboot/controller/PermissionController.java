6192879a3cc1339a057d3c5df8097ec4252935e5
yinchong <yin.chong@intellif.com>
2019-07-05 20:57:14 +0800

    引入layer的使用

--- src/main/java/cn/bucheng/shiroboot/controller/PermissionController.java
+++ src/main/java/cn/bucheng/shiroboot/controller/PermissionController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 
 /**
  * @author ：yinchong
- * @create ：2019/7/5 19:18
- * @description：角色控制
+ * @create ：2019/7/5 20:56
+ * @description：权限管理控制器
  * @modified By：
  * @version:
  */
 @Controller
-public class RoleController {
+public class PermissionController {
 }
